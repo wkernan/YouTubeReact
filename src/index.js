ec813e3302319a1e0f67036973ead6ed2544e463
Bill <wkernan@gmail.com>
2017-01-02 22:12:18 -0600

    boilerplate plus initial search functionality

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyCmHciIYevFUC4SNQpTO-cB_Shtl0miN-I';
 
-import App from './components/app';
-import reducers from './reducers';
+// create a new component, should produce some html
+class App extends Component {
+	constructor(props) {
+		super(props);
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+		this.state = { videos: [] };
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+		YTSearch({key: API_KEY, term: 'overwatch'}, (videos) => {
+			//es6 makes us able to change this.setState({ videos: videos}) to ({ videos }), since key and property are same name
+			this.setState({ videos }); 
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoDetail video={this.state.videos[0]} />
+				<VideoList videos={this.state.videos} /> 
+			</div>
+		);
+	}
+}
+
+// take the component's generated html and put it on the page(dom)
+ReactDOM.render(<App />, document.querySelector('.container'));
